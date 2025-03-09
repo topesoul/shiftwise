b33fd8abfcacfb3ab0ccd8cf3c3124b8b09f5a0b
topesoul <130389038+topesoul@users.noreply.github.com>
2025-03-09 15:03:04 +0000

    feat: add subscriptionSyncMiddleware to settings

--- shiftwise/settings.py
+++ shiftwise/settings.py
@@ -93,6 +93,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "subscriptions.middleware.SubscriptionSyncMiddleware",
 ]
 
 ROOT_URLCONF = "shiftwise.urls"
